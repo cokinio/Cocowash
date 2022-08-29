1dd0135090602e2d08009873fce07707dcb9ab02
Leandro Schlain <cokinio@gmail.com>
2022-08-29 14:26:25 -0300

    se crearon nuevas funciones

--- js/calculo.js
+++ js/calculo.js
@@ -68,6 +68,104 @@ function funTotal(carrito) {
 	return total;
 }
 
+function realizarPedido(){
+	////consulto por remeras
+	
+	cantRemeras = parseInt(
+		prompt("Ingrese la cantidad de remeras que desea lavar")
+	);
+	
+	for (i = 0; i < cantRemeras; i++) {
+		talle = prompt(
+			"Ingrese el talle de la remera. Puede ser small [s], medium [m] o large [l]. Deberá utilizar la letra indicada dentro de los corchetes para indicar el talle"
+		);
+		while (talle != "s" && talle != "m" && talle != "l") {
+			talle = prompt(
+				"Parametro incorrecto, ingrese nuevamente el talle de la remera. Puede ser s, m o l"
+			);
+		}
+		color = prompt("Ingrese el color de la remera");
+		const remera1 = new Remera(talle, color);
+		cliente1.carrito.push(remera1);
+	}
+	
+	//consulto por pantalones
+	cantPantalones = parseInt(
+		prompt("Ingrese la cantidad de pantalones que desea lavar")
+	);
+	
+	for (i = 0; i < cantPantalones; i++) {
+		talle = prompt(
+			"Ingrese el talle del pantalon. Puede ser small [s], medium [m] o large [l]. Deberá utilizar la letra indicada dentro de los corchetes para indicar el talle"
+		);
+		while (talle != "s" && talle != "m" && talle != "l") {
+			talle = prompt(
+				"Parametro incorrecto, ingrese nuevamente el talle del pantalon. puede ser s, m o l"
+			);
+		}
+		color = prompt("Ingrese el color del pantalon");
+		const pantalon1 = new Pantalon(talle, color);
+		cliente1.carrito.push(pantalon1);
+	}
+	
+	//consulto por Buzos
+	cantBuzos = parseInt(prompt("Ingrese la cantidad de buzos que desea lavar"));
+	
+	for (i = 0; i < cantBuzos; i++) {
+		talle = prompt(
+			"Ingrese el talle del buzo. Puede ser small [s], medium [m] o large [l]. Deberá utilizar la letra indicada dentro de los corchetes para indicar el talle"
+		);
+		while (talle != "s" && talle != "m" && talle != "l") {
+			talle = prompt(
+				"Parametro incorrecto, ingrese nuevamente el talle de la remera. puede ser s, m o l"
+			);
+		}
+		color = prompt("Ingrese el color del buzo");
+		const buzo1 = new Buzo(talle, color);
+		cliente1.carrito.push(buzo1);
+	}
+}
+
+function escribirMensaje(cantidadProductos,cantidades,singular,plural){
+	let mensaje ="El cliente " + cliente1.nombre + " " + cliente1.apellido + " desea lavar ";
+	for (i = 0; i < cantidadProductos; i++) {
+		if (cantidades[i].length == 0) {
+			mensaje = mensaje + cantidades[i].length + plural[i];
+		} else if (cantidades[i].length == 1) {
+			mensaje =
+				mensaje +
+				cantidades[i].length +
+				singular[i] +
+				"de color " +
+				cantidades[i][0].color;
+		} else {
+			mensaje = mensaje + cantidades[i].length + plural[i] + "de colores ";
+			for (j = 0; j < cantidades[i].length; j++) {
+				if (j < cantidades[i].length - 2) {
+					mensaje = mensaje + cantidades[i][j].color + ", ";
+				} else if (j < cantidades[i].length - 1) {
+					mensaje = mensaje + cantidades[i][j].color + " y ";
+				} else {
+					mensaje = mensaje + cantidades[i][j].color;
+				}
+			}
+		}
+		// veo si poner coma ,y , o espacio en el mensaje
+		if (i < cantidadProductos - 2) {
+			mensaje = mensaje + ", ";
+		} else if (i < cantidadProductos - 1) {
+			mensaje = mensaje + " y ";
+		} else {
+			mensaje = mensaje + " ";
+		}
+	}
+	
+	mensaje = mensaje + "por un total de $" + funTotal(cliente1.carrito);
+	alert(mensaje);
+	}
+
+
+
 alert(
 	"En este programa le cotizaremos las prendas que desea lavar en base a su descripción"
 );
@@ -77,110 +175,14 @@ let apellido = prompt("Ingrese su apellido");
 let carrito = [];
 const cliente1 = new Cliente(nombre, apellido, carrito);
 
-///////////////////// consulto por el pedido ///////////////////////////
-
-////consulto por remeras
-
-cantRemeras = parseInt(
-	prompt("Ingrese la cantidad de remeras que desea lavar")
-);
-
-for (i = 0; i < cantRemeras; i++) {
-	talle = prompt(
-		"Ingrese el talle de la remera. Puede ser small [s], medium [m] o large [l]. Deberá utilizar la letra indicada dentro de los corchetes para indicar el talle"
-	);
-	while (talle != "s" && talle != "m" && talle != "l") {
-		talle = prompt(
-			"Parametro incorrecto, ingrese nuevamente el talle de la remera. Puede ser s, m o l"
-		);
-	}
-	color = prompt("Ingrese el color de la remera");
-	const remera1 = new Remera(talle, color);
-	cliente1.carrito.push(remera1);
-}
-
-//consulto por pantalones
-cantPantalones = parseInt(
-	prompt("Ingrese la cantidad de pantalones que desea lavar")
-);
-
-for (i = 0; i < cantPantalones; i++) {
-	talle = prompt(
-		"Ingrese el talle del pantalon. Puede ser small [s], medium [m] o large [l]. Deberá utilizar la letra indicada dentro de los corchetes para indicar el talle"
-	);
-	while (talle != "s" && talle != "m" && talle != "l") {
-		talle = prompt(
-			"Parametro incorrecto, ingrese nuevamente el talle del pantalon. puede ser s, m o l"
-		);
-	}
-	color = prompt("Ingrese el color del pantalon");
-	const pantalon1 = new Pantalon(talle, color);
-	cliente1.carrito.push(pantalon1);
-}
-
-//consulto por Buzos
-cantBuzos = parseInt(prompt("Ingrese la cantidad de buzos que desea lavar"));
-
-for (i = 0; i < cantBuzos; i++) {
-	talle = prompt(
-		"Ingrese el talle del buzo. Puede ser small [s], medium [m] o large [l]. Deberá utilizar la letra indicada dentro de los corchetes para indicar el talle"
-	);
-	while (talle != "s" && talle != "m" && talle != "l") {
-		talle = prompt(
-			"Parametro incorrecto, ingrese nuevamente el talle de la remera. puede ser s, m o l"
-		);
-	}
-	color = prompt("Ingrese el color del buzo");
-	const buzo1 = new Buzo(talle, color);
-	cliente1.carrito.push(buzo1);
-}
-
-console.log(cliente1);
-
+realizarPedido();
 let arrPantalon = cliente1.carrito.filter(esPantalon);
 let arrRemera = cliente1.carrito.filter(esRemera);
 let arrBuzo = cliente1.carrito.filter(esBuzo);
 const cantidadProductos = 3;
-// armo el mensaje a mostrar en consola
+let cantidades = [arrPantalon, arrRemera, arrBuzo],
+	singular = [" pantalon ", " remera ", " buzo "],
+	plural = [" pantalones ", " remeras ", " buzos "];
 
-let mensaje =
-	"El cliente " + cliente1.nombre + " " + cliente1.apellido + " desea lavar ";
+escribirMensaje(cantidadProductos,cantidades,singular,plural);
 
-let cantidades = [arrPantalon, arrRemera, arrBuzo];
-let singular = [" pantalon ", " remera ", " buzo "];
-let plural = [" pantalones ", " remeras ", " buzos "];
-
-for (i = 0; i < cantidadProductos; i++) {
-	if (cantidades[i].length == 0) {
-		mensaje = mensaje + cantidades[i].length + plural[i];
-	} else if (cantidades[i].length == 1) {
-		mensaje =
-			mensaje +
-			cantidades[i].length +
-			singular[i] +
-			"de color " +
-			cantidades[i][0].color;
-	} else {
-		mensaje = mensaje + cantidades[i].length + plural[i] + "de colores ";
-		for (j = 0; j < cantidades[i].length; j++) {
-			if (j < cantidades[i].length - 2) {
-				mensaje = mensaje + cantidades[i][j].color + ", ";
-			} else if (j < cantidades[i].length - 1) {
-				mensaje = mensaje + cantidades[i][j].color + " y ";
-			} else {
-				mensaje = mensaje + cantidades[i][j].color;
-			}
-		}
-	}
-	// ve si poner coma o un y en el mensaje
-	if (i < cantidadProductos - 2) {
-		mensaje = mensaje + ", ";
-	} else if (i < cantidadProductos - 1) {
-		mensaje = mensaje + " y ";
-	} else {
-		mensaje = mensaje + " ";
-	}
-}
-
-mensaje = mensaje + "por un total de $" + funTotal(cliente1.carrito);
-alert(mensaje);
